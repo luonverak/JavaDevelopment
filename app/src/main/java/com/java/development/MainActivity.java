cc434339e99c067b07a454d96a83ec78a97a9450
Verak Luon <luonverakcambo@gmail.com>
2025-02-02 11:03:58 +0700

    view binding

---
+++ app/src/main/java/com/java/development/MainActivity.java
@@ -0,0 +1,32 @@
+package com.java.development;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.java.development.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Initialize view binding
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.clickToDetail.setOnClickListener(
+                v -> startActivity(new Intent(this, DetailActiivity.class))
+        );
+    }
+}
