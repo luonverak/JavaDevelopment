dd10584b80c602cf28492572b6fc07ada6b77e6f
verak-blue <luonverakcambo@gmail.com>
2025-02-15 19:00:36 +0700

    design

--- app/src/main/java/com/java/development/MainActivity.java
+++ app/src/main/java/com/java/development/MainActivity.java
@@ -1,5 +1,6 @@
 package com.java.development;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -37,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapterUser);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        binding.tbnAdd.setOnClickListener(v-> {
+            startActivity(new Intent(this, DetailActivity.class));
+            finish();
+        });
 
     }
 }
